ada790a1de01eed5cf367c5a2b72b2d97889b5b3
Yandi <yandivd@gmail.com>
2022-08-05 20:45:40 -0400

    modelos hechos

--- app/admin.py
+++ app/admin.py
@@ -7,5 +7,23 @@ class TrabajadorAdmin(admin.ModelAdmin):
 class MunicipioAdmin(admin.ModelAdmin):
     list_display=('nombre',)
 
+class Unidad_OrganizativaAdmin(admin.ModelAdmin):
+    list_display=('nombre',)
+
+class SolicitudAdmin(admin.ModelAdmin):
+    list_display=(
+        'solicitante',
+        'unidad_organizativa',
+        'c_contable',
+        'origen',
+        'destino',
+        'regreso',
+        'fecha_inicio',
+        'fecha_final'
+    )
+
 # Register your models here.
 admin.site.register(Trabajador, TrabajadorAdmin)
+admin.site.register(Municipio, MunicipioAdmin)
+admin.site.register(Solicitud, SolicitudAdmin)
+admin.site.register(Unidad_Organizativa, Unidad_OrganizativaAdmin)
--- app/models.py
+++ app/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Trabajador(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     ci=models.CharField(max_length=11, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return self.user.username
 
 class Unidad_Organizativa(models.Model):
     nombre=models.CharField(max_length=100)
@@ -29,8 +30,9 @@ class Solicitud(models.Model):
     unidad_organizativa=models.ForeignKey(Unidad_Organizativa, on_delete=models.CASCADE)
     c_contable=models.CharField(max_length=4)
     #provincia=models.ForeignKey(Provincia, on_delete=models.CASCADE)
-    origen=models.ForeignKey(Municipio, on_delete=models.CASCADE)
-    #regreso=models.ForeignKey(Municipio, on_delete=models.CASCADE)
+    origen=models.ForeignKey(Municipio, on_delete=models.CASCADE, related_name='municipio_origen')
+    destino=models.ForeignKey(Municipio, on_delete=models.CASCADE, related_name='municipio_destino')
+    regreso=models.ForeignKey(Municipio, on_delete=models.CASCADE, related_name='municipio_regreso')
     fecha_inicio=models.DateField()
     fecha_final=models.DateField()
     
