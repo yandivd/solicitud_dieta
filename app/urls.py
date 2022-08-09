82d319b85612c471194c141e90c493a26158cc77
Yandi <yandivd@gmail.com>
2022-08-09 16:34:38 -0400

    listado de pendientes v2

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from .views import SolicitudListView, SolicitudCreateView, SolicitudPendienteLis
 urlpatterns = [
     path('', SolicitudListView.as_view(), name='solicitudes' ),
     path('crear_solicitud/', SolicitudCreateView.as_view(), name='crear_solicitud'),
-    path('listar_solicitud/', SolicitudPendienteListView.as_view(), name='pendientes'),
+    path('solicitudes_pendientes/', SolicitudPendienteListView.as_view(), name='pendientes'),
 
 ]
